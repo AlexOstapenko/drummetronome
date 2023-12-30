31b4fe08e6b83a3195b7d8e163c7fb21a0453ea6
Alex Ostapenko <aostapenko2000@yahoo.com>
2023-12-30 23:38:57 +0530

    design fixes

--- public/js/main/courses-main.js
+++ public/js/main/courses-main.js
@@ -2,7 +2,7 @@
 * GLOBAL VARIABLES
 */
 const courseRunner = new CourseRunner();
-const openByDefault = {course: "frame-drums-ru", module: "module_snaps_1", lesson: "lesson-1.html"};
+const openByDefault = {course: "frame-drums-ru", module: "module_snaps_2", lesson: "lesson-1.html"};
 
 function onDocumentLoaded() {
 	instrumentManager.loadInstrumentDefinitions( function() {
